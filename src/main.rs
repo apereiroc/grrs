81e4b1eea76da60e9dc34b6ff3e4c15115c922f2
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-01-11 00:36:49 +0100

    Parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+// Derived from clap::Parser
+// To store the command line arguments
+// Clap knows the expected fields and their format
+// It automatically generates a help message
+#[derive(Parser)]
+struct Cli {
+    // The pattern to look for
+    pattern: String,
+    // The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
